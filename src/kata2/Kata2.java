417c01bd1b6bb014b98c257f7baf5d243b89574a
usuario <usuario@10.22.143.60>
2016-09-30 09:36:36 +0100

    vERSION4

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -1,15 +1,15 @@
 //Alberto Hernández Rodríguez doble grado
 package kata2;
-import java.util.HashMap;
 import java.util.Map;
 
 public class Kata2 {
 
     public static void main(String[] args) {
-        int[]data={1,1,1,100,2,4,5,6,8,8,4,5,6,2,-4};
+        //Integer[]data={1,1,1,100,2,4,5,6,8,8,4,5,6,2,-4};
+        String[] data={"pepe","pepe","ana","pepe","ana","rosa","juan"};
         Histogram histo=new Histogram(data);
-        Map<Integer,Integer> histogr=histo.getHistogram();
-        for(int key:histogr.keySet()){
+        Map<String,Integer> histogr=histo.getHistogram();
+        for(String key:histogr.keySet()){
             System.out.println(key+"-->"+histogr.get(key));
         }
     }
